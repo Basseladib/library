63ec457ec95931eb270d4ba80d60a512868eeb93
Bassel Adel <bassel.adib123@gmail.com>
2024-04-18 16:11:41 +0200

    first commit with user, books and library

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    using System;
+
+    
+    
+        public class Book
+        {
+            public int Index { get; set; }
+            public string Title { get; set; }
+
+            public Book(string title, int i)
+            {
+                Index = i;
+                Title = title;
+            }
+        }
+    
+}
